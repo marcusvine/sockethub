21cc41477ede00e4d6d59dc3322ff5ef82636770
aaryadev <devmgbhat@gmail.com>
2020-04-17 14:53:51 +0530

    reset from base

--- server.js
+++ server.js
@@ -1,70 +1,20 @@
+'use strict';
 
-var static = require('node-static');
-var file = new static.Server('./UI');
+const express = require('express');
+const socketIO = require('socket.io');
 
-//vars
-var userCount=0;
-var chanels=[];
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
 
-//Define
-var server = require('http').createServer(function(request, response) {
-    request.addListener('end', function() {
-        file.serve(request, response);
-    }).resume();
-});
-var io =require('socket.io')(server);
-server.listen(process.env.PORT || 9999);
-console.log("Server Started");
-//connection
-io.on('connection',function(socket){
-	userCount++; // user+
-	sysinfo();
-	var current_chanel=false;
-	var user=null;
-	//join chanel
-	socket.on('join',function(data){
-		if(!chanels[data.chanel_id]){
-			//new chanel
-			chanels[data.chanel_id]={
-				 users: []
-			}
-		}
-		user ={
-			id: chanels[data.chanel_id].users.length + 1,
-            name: data.name,
-            socket_id: socket.id
-		}
-		chanels[data.chanel_id].users.push(user); //add user
-		socket.join(data.chanel_id); //join socket
-		current_chanel = data.chanel_id; //add to current
-		io.to(current_chanel).emit('user_joined',user);
-		sysinfo(); //update to all
-	});
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-	//on data  relay to chanel
-	socket.on('data',function(data){
-		io.to(current_chanel).emit('data',data);
-	});
+const io = socketIO(server);
 
-
-	//on disconnect
-	socket.on('disconnect', function () {
-		userCount--; //user -
-		sysinfo();
-		if(current_chanel){
-			socket.leave(current_chanel);
-			io.to(current_chanel).emit('user_left',user);
-			chanelCleanup(current_chanel,user);
-		}
-	});
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-//////////////////////PUBLIC FUNCTION////////////////
-var sysinfo = function(){
-	io.sockets.emit('sysinfo',{userCount:userCount,chanelCount:Object.keys(chanels).length});
-}
-
-var chanelCleanup = function(chanel_id,user){
-	//remove user from array
-	chanels[chanel_id].users.splice((user.id-1),1); //remove current user from chanel user array
-}
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
