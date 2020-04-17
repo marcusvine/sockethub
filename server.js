6d05b17044a7133a8809374ff2154532a0618800
aaryadev <devmgbhat@gmail.com>
2020-04-17 21:29:29 +0530

    opps :|

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+
 const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
@@ -8,7 +9,7 @@ const INDEX = '/index.html';
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => //console.log(`Listening on ${PORT}`));
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
 
